77cc0d026dd6c671c108a63dfeab494d4310ba39
Danky <oregonjonathan@gmail.com>
2022-08-05 00:44:10 -0700

    add basic arithmethic functions

---
+++ js-main.js
@@ -0,0 +1,19 @@
+const add = function(num1, num2) {
+    return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+    return num1 - num2;
+};
+
+const multiply = function(num1, num2) {
+    return num1 * num2;
+  };
+  
+  const divide = function(num1, num2) {
+    return Math.round((num1 / num2)*10)/10;
+  };
+  
+  console.log(add(2, 5));
+
+

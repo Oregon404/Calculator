bf88df5a1312ebac6babc9c34e41223836ecfeff
Danky <oregonjonathan@gmail.com>
2022-08-06 06:54:23 -0700

    fix crash form dividing by 0

--- js-main.js
+++ js-main.js
@@ -1,22 +1,80 @@
+let crntNum = '';
+let prevNum = '';
+let firstNum = '';
+let total = '';
+let op= '';
+
+
+const crntDisNum = document.querySelector('.disVal');
+
+const equal = document.querySelector('.equal');
+equal.addEventListener('click',operate);
+
+const funcBtns = document.querySelectorAll('.func');
+
+const numBtns = document.querySelectorAll('.num');
+
+numBtns.forEach((btn) => {
+    btn.addEventListener('click',(e)=>{
+        getNum(e.target.textContent);
+    });
+});
+
+funcBtns.forEach((btn) => {
+    btn.addEventListener('click',(e)=>{
+        getFunc(e.target.textContent);
+    });
+});
+
+function getFunc(func){
+    if( total != ''){
+        crntNum = total;
+        total = '';
+    }
+    op=func;
+    firstNum = crntNum;
+    prevNum = crntNum + '' + op + '';
+    crntNum = '';
+    crntDisNum.textContent = prevNum;
+
+}
+
+function getNum(num){
+    if(crntNum.length < 6){
+        crntDisNum.textContent = prevNum;
+        crntNum+=num;
+        crntDisNum.textContent = prevNum + crntNum;
+    }
+}
+
 const add = function(num1, num2) {
-    console.log(num1 + num2);
-    return num1 + num2;
+    total = num1 + num2;
+    crntDisNum.textContent = total;
 };
 
 const subtract = function(num1, num2) {
-    return num1 - num2;
+    total=  num1 - num2;
+    crntDisNum.textContent = total;
 };
 
 const multiply = function(num1, num2) {
-    return num1 * num2;
+    total=  num1 * num2;
+    crntDisNum.textContent = total;
   };
   
   const divide = function(num1, num2) {
-    return Math.round((num1 / num2)*10)/10;
+    if(num1 === 0 || num2 === 0 ){
+        zeroOut();
+        crntDisNum.textContent = 'To infinity & beyond!!';
+    } else {total=  Math.round((num1 / num2)*10)/10;
+    crntDisNum.textContent = total;
+}
   };
 
-  function operate(num1,op, num2){
-    console.log(op);
+
+  function operate(num1,num2){
+    num1= Number(firstNum);
+    num2= Number(crntNum);
     if (op === '+'){
         add(num1, num2);
     } else if (op === '-'){
@@ -30,3 +88,17 @@ const multiply = function(num1, num2) {
         
     }
   };
+
+  const zeroOut= function(){
+    crntNum = '';
+    prevNum = '';
+    firstNum = '';
+    total = '';    
+    op = '';
+    crntDisNum.textContent = '';
+
+}
+
+const clear = document.querySelector('.ac');
+clear.addEventListener('click',zeroOut);
+  

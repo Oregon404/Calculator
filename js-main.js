a239e249c79e170aa354d5a0e28bfe9e553cfeea
Danky <oregonjonathan@gmail.com>
2022-08-05 21:56:15 -0700

    add buttons to css

--- js-main.js
+++ js-main.js
@@ -1,4 +1,5 @@
 const add = function(num1, num2) {
+    console.log(num1 + num2);
     return num1 + num2;
 };
 
@@ -13,7 +14,19 @@ const multiply = function(num1, num2) {
   const divide = function(num1, num2) {
     return Math.round((num1 / num2)*10)/10;
   };
-  
-  console.log(add(2, 5));
 
-
+  function operate(num1,op, num2){
+    console.log(op);
+    if (op === '+'){
+        add(num1, num2);
+    } else if (op === '-'){
+        subtract(num1, num2);
+    } else if (op === '*'){
+        multiply(num1, num2);
+    } else if(op === '/') {
+        divide(num1, num2);
+    }else {
+        return 'error';
+        
+    }
+  };
